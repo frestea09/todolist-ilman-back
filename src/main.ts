b7c0e8ba5163dfa94df914fe230df9f270d241b9
ilman <ilmanfrasetya@gmail.com>
2025-03-03 23:59:48 +0700

    vercel

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+  app.enableCors(); // Penting untuk akses API dari frontend lain
   app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(process.env.PORT ?? 3000);
 }
